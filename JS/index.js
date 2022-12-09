e1d69d85891b793808e844a46eefc0b633cd75ff
Yuvaraj Naidu <githubyuvaraj@gmail.com>
2022-12-09 22:23:37 +0530

    v3

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,12 @@
-(function() {
+  (function() {
     $('.btn').click(function() {
       $(this).toggleClass('active');
       return $('.box').toggleClass('open');
     });
   
-  }).call(this);  
+  }).call(this); 
+
+  // Loading page
+	var loaderPage = function() {
+		$(".fh5co-loader").fadeOut("slow");
+	};
